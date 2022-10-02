dfd93f6c14856753798c2c381e123947483ea45a
Kaspar Alexander <kaspar.alexander@gmail.com>
2022-10-02 16:05:02 -0400

    first time slot input

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,20 @@
-const dayjs = require('dayjs')
-//import dayjs from 'dayjs' // ES 2015
-dayjs().format()
+// use Moment to display current date in header
+$("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
+
+// get event info from user input and keep in local storage
+/// user types into text area, then clicks save. When user clicks save, get the parent div hour and store hour + input
+var newEvent;
+var newTime;
+
+$(".saveBtn").click(function () {
+    newTime = $(this).parent().attr("id");
+    console.log(newTime);
+    newEvent = $(this).siblings(".eventName").val();
+    console.log(newEvent);
+})
+
+// dynamically display different colors based on current time
+/// define past, current, and future time. Past is grey, current is red, future is green
+
+
+// load previously saved events from local storage
